6a250b4571c85742987a99d36c8c7fce7e85e0fe
adexoludare <adexoludare@gmail.com>
2022-12-12 17:54:13 -0800

    update code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-
-
-/**
+/*
  *
  *  * main - prints the alphabet in lowercase,
  *
@@ -18,8 +16,6 @@ int main(void)
 
 		char ch;
 
-
-
 			for (ch = 'a'; ch <= 'z'; ch++)
 
 					{
@@ -33,46 +29,4 @@ int main(void)
 					return (0);
 
 }
-
-
-
-3. alphABET
-
-#include <stdio.h>
-
-
-
-/**
- *
- *  * main - prints the alphabet in lowercase, and then in uppercase,
- *
- *   * followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-		int ch;
-
-
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-						putchar(ch);
-
-				for (ch = 'A'; ch <= 'Z'; ch++)
-
-							putchar(ch);
-
-					putchar('\n');
-
-						return (0);
-
-}
-
-
 
