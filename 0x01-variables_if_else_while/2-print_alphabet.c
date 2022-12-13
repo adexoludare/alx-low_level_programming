6f5b2ce4ff69bfe1c351aa4cdd7334e7fe530a8b
adexoludare <adexoludare@gmail.com>
2022-12-12 17:14:01 -0800

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the alphabet in lowercase,
+ *
+ *   * followed by a new line
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+
+					{
+
+								putchar(ch);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
+
+
+
+3. alphABET
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the alphabet in lowercase, and then in uppercase,
+ *
+ *   * followed by a new line
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+
+						putchar(ch);
+
+				for (ch = 'A'; ch <= 'Z'; ch++)
+
+							putchar(ch);
+
+					putchar('\n');
+
+						return (0);
+
+}
+
+
+
