32a244fd5f2dcd5bfdadc93e203cbaf18e10d621
adexoludare <adexoludare@gmail.com>
2022-12-22 15:00:13 +0100

    0x03-debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,25 +1,12 @@
-positive_or_negative.c
+#ifndef MAIN_H
+#define MAIN_H
+
+
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Description: print value of n status; zero, positive or negative
- * Return: Always 0 (Success)
-*/
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int month, int day);
 
-void positive_or_negative(int i)
-{
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-}
-
+#endif /* MAIN_H */
