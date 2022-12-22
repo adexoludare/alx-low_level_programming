a545613c0f1ceb8b7fcf936c9d3b8a1726871044
adexoludare <adexoludare@gmail.com>
2022-12-22 14:27:19 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,8 +3,8 @@
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *	    the byte position, then showing the hex content,
- *	    then displaying printable charcaters.
+ * the byte position, then showing the hex content,
+ * then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
@@ -22,26 +22,23 @@ void print_buffer(char *b, int size)
 				printf("  ");
 			else
 				printf("%02x", *(b + index + byte));
-
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
 				break;
 			else if (*(b + index + byte) >= 31 &&
-			       *(b + index + byte) <= 126)
-			      printf("%c", *(b + index + byte));
+					*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
 			else
 				printf(".");
 		}
 		if (byte >= size)
 			continue;
-
 		printf("\n");
 	}
 	if (size <= 0)
-		printf("\n");
+	printf("\n");
 }
